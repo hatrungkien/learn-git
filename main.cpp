8559c3ee03ce07f4484b12014519b88361e20d15
kien <thangconnguoita@gmail.com>
2021-11-24 10:37:01 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     std::cout<<"hello master"<<std::endl;
+    std::cout<<"this is master local"<<std::endl;
+    std::cout<<"hello master"<<std::endl;
     std::cout<<"this is master nene"<<std::endl;
     std::cout<<"hello kien-branch"<<std::endl;
     std::cout<<"how r u"<<std::endl;
