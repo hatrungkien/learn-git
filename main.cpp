7c091bc57b8b752c96278e3fb07d8f881033b3a0
kien <thangconnguoita@gmail.com>
2021-11-24 10:49:07 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main() {
 
     std::cout<<"hello master"<<std::endl;
     std::cout<<"this is master local"<<std::endl;
-    std::cout<<"hello master"<<std::endl;
-    std::cout<<"this is master nene"<<std::endl;
     std::cout<<"hello kien-branch"<<std::endl;
     std::cout<<"how r u"<<std::endl;
 
