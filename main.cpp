61c87b8d08ba3f18779ee780dd47cc912098a1fc
kien <thangconnguoita@gmail.com>
2021-11-24 10:37:01 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     std::cout<<"hello master"<<std::endl;
+    std::cout<<"this is master local"<<std::endl;
+    std::cout<<"hello master"<<std::endl;
     std::cout<<"this is rebase"<<std::endl;
     std::cout<<"kien-branch"<<std::endl;
     std::cout<<"onto master"<<std::endl;
