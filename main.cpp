580fa09a0e70138de8f5939c0fd0e2b7e6a619c6
kien <thangconnguoita@gmail.com>
2021-11-24 10:34:45 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "iostream"
 
 int main() {
+    std::cout<<"hello kien-branch"<<std::endl;
+    std::cout<<"how r u"<<std::endl;
+
     std::cout<<"hello master"<<std::endl;
     std::cout<<"this is master local"<<std::endl;
     std::cout<<"hello master"<<std::endl;
