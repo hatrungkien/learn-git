7fd8f4731094145541152f333b8c0f40d7f9bb83
kien <thangconnguoita@gmail.com>
2021-11-25 08:44:14 +0700

    add code and fix main

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout<<"hello master"<<std::endl;
     std::cout<<"this is master local"<<std::endl;
     std::cout<<"hello master"<<std::endl;
-    std::cout<<"this is kien branch"<<std::endl;std::cout<<"hello master"<<std::endl;
+    std::cout<<"this is kien branch"<<std::endl;
+    std::cout<<"hello master"<<std::endl;
 }
