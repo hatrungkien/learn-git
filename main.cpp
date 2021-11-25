5079796a3a94626568a632a56c898bd23429e324
kien <thangconnguoita@gmail.com>
2021-11-25 08:33:07 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include "iostream"
 
 int main() {
-    std::cout<<"hello kien-branch"<<std::endl;
-    std::cout<<"how r u"<<std::endl;
-
     std::cout<<"hello master"<<std::endl;
     std::cout<<"this is master local"<<std::endl;
     std::cout<<"hello master"<<std::endl;
-    std::cout<<"this is rebase"<<std::endl;
-    std::cout<<"kien-branch"<<std::endl;
-    std::cout<<"onto master"<<std::endl;
-std::cout<<"hello kien-branch"<<std::endl;
-    std::cout<<"how r u"<<std::endl;
+    std::cout<<"this is kien branch"<<std::endl;std::cout<<"hello master"<<std::endl;
 }
